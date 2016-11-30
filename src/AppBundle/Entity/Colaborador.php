288cf4dd41ab0fad3e9bfc7a88dd2c9e8803182f
Mario Quesada <marioquesada2@gmail.com>
2016-11-30 10:36:30 +0100

    Gestion de archivos dentro de la entidad

--- src/AppBundle/Entity/Colaborador.php
+++ src/AppBundle/Entity/Colaborador.php
@@ -1,11 +1,11 @@
 <?php
 
 
-
+//To Do Mejorar gestion imagen
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 /**
 * @ORM\Entity
 * @ORM\Table(name="Colaboradores")
@@ -19,14 +19,14 @@ class Colaborador
     private $id;
 
     /**
+    * @ORM\Column(type="string")
+    */
+    private $rol;
+    /**
     * @ORM\Column(type="string",length=255)
     */
     private $nombre;
 
-    /**
-    * @ORM\Column(type="string")
-    */
-    private $uri;
 
     /**
     * @ORM\Column(type="text")
@@ -36,76 +36,39 @@ class Colaborador
     /**
     * @ORM\Column(type="string")
     */
-    private $rol;
-
+    private $path;
 
     /**
-     * Set nombre
+    * @ORM\Column(type="blob")
+    */
+    private $file;    
+
+    /**
+    * @ORM\Column(type="datetime")
+    */
+    private $date;
+
+    /**
+     * Set id
      *
-     * @param string $nombre
+     * @param string $id
      * @return Colaborador
      */
-    public function setNombre($nombre)
+    public function setId($id)
     {
-        $this->nombre = $nombre;
+        $this->id = $id;
 
         return $this;
     }
 
     /**
-     * Get nombre
+     * Get id
      *
      * @return string 
      */
-    public function getNombre()
+    public function getId()
     {
-        return $this->nombre;
-    }
-
-    /**
-     * Set uri
-     *
-     * @param string $uri
-     * @return Colaborador
-     */
-    public function setUri($uri)
-    {
-        $this->uri = $uri;
-
-        return $this;
-    }
-
-    /**
-     * Get uri
-     *
-     * @return string 
-     */
-    public function getUri()
-    {
-        return $this->uri;
-    }
-
-    /**
-     * Set description
-     *
-     * @param string $description
-     * @return Colaborador
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
-    /**
-     * Get description
-     *
-     * @return string 
-     */
-    public function getDescription()
-    {
-        return $this->description;
+        return $this->id;
     }
 
     /**
@@ -132,25 +95,132 @@ class Colaborador
     }
 
     /**
-     * Set id
+     * Set nombre
      *
-     * @param string $id
+     * @param string $nombre
      * @return Colaborador
      */
-    public function setId($id)
+    public function setNombre($nombre)
     {
-        $this->id = $id;
+        $this->nombre = $nombre;
 
         return $this;
     }
 
     /**
-     * Get id
+     * Get nombre
      *
      * @return string 
      */
-    public function getId()
+    public function getNombre()
     {
-        return $this->id;
+        return $this->nombre;
     }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Colaborador
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Colaborador
+     */
+    public function setPath()
+    {
+
+        $this->path = "files/images/patners/".$this->id.".".$this->file->getClientOriginalExtension();
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set file
+     *
+     * @param \file $file
+     * @return Colaborador
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return \file 
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Colaborador
+     */
+    public function setDate()
+    {
+        $this->date = new \DateTime("now");;
+
+        return $this;
+    }
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+    
+    public function upload()
+    {
+        dump($this);
+        $this->getFile()->move("/var/www/congreso/files/images/patners/",$this->id.".".$this->file->getClientOriginalExtension());
+        $this->setFile(file_get_contents($this->path));
+        dump($this);
+
+    }
+
+    public function deleteFile()
+    {
+        return unlink($this->path);
+    }
+
 }

53a9aee08ebddc0a5af02d2a8ec0265c0bbe7d69
Mario Quesada <marioquesada2@gmail.com>
2017-01-20 12:30:18 +0100

    fecha de publicación

--- src/AppBundle/Controller/AsistenteController.php
+++ src/AppBundle/Controller/AsistenteController.php
@@ -313,18 +313,22 @@ class AsistenteController extends Controller
 			->add("polity","checkbox",array("label"=>'He leido y acepto las <a href="/politica-de-privacidad">condiciones</a>',
 				"required" => True,"attr"=> array("class"=>"opciones")))
 			->add("public","checkbox",array("label"=>"Permito que mi asistencia se publique en el listado de asistentes",
-				"required" => false,"attr"=> array("class"=>"opciones")))
+				"required" => false,"attr"=> array("class"=>"opciones")));
 /**
 
 
 
 */
-
-			/*->add('save', 'submit', array(
+			if((time()>=strtotime("24-01-2017 13:00:00"))&&(time()<=strtotime("08-02-2017 00:00:00")))
+			{
+				$form->add('save', 'submit', array(
 				'label' => 'Guardar',
 				"attr" => array("class" => "btn btn-primary")
-				))*/
-				;
+				));
+			}
+			
+			
+			
 		return $form;
 	}
 
